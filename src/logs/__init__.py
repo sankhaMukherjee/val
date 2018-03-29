5a107be5b8e1fb3cff1e55551ab52abfdad4cbd9
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2018-03-29 09:08:45 +0800

    This is the first commit

---
+++ src/logs/__init__.py
@@ -0,0 +1,7 @@
+'''module containing helper functions for logging
+
+This module contains two functions. The first one 
+will be used for generating a new logger object, 
+and another one for uisng that logging object
+for new tasks. 
+'''

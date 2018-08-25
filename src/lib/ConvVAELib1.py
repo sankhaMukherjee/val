d83b92af9444f90d46d036d078031f0ac437dead
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2018-08-25 20:18:37 +0800

    now we have a new module for this

---
+++ src/lib/ConvVAELib1.py
@@ -0,0 +1,275 @@
+from tqdm     import tqdm
+from logs     import logDecorator as lD
+from datetime import datetime     as dt
+
+import json, os
+import numpy      as np
+import tensorflow as tf
+
+
+config = json.load(open('../config/config.json'))
+logBase = config['logging']['logBase'] + '.lib.ConvVAELib1.ConvVAE'
+
+class ConvVAE():
+
+    @lD.log(logBase + '.AE.__init__')
+    def __init__(logger, self, 
+            nInpX, nInpY, nInpCh, 
+            filters, kernels, strides, activations, 
+            nLatent, L=1.5):
+
+        self.nInpX   = nInpX
+        self.nInpY   = nInpY
+        self.nInpCh  = nInpCh
+        self.nLatent = nLatent
+        self.L       = L
+        self.restorePoints = []
+
+        self.Inp  = tf.placeholder(
+                        dtype=tf.float32, 
+                        shape=(None, nInpX, nInpY, nInpCh))
+
+        self.Inp1 = tf.reshape(self.Inp, (-1, nInpX*nInpY))
+
+        # ------------------------------------------------------------
+        # Generate the encoder network
+        # ------------------------------------------------------------
+        self.encoder = None
+        for i, (f, k, s, a) in enumerate(zip(filters, kernels, strides, activations)):
+            if i == 0:
+                self.encoder = tf.layers.conv2d(
+                    self.Inp, filters = f, 
+                    kernel_size = k, strides = s, padding = 'same',
+                    activation = a)
+            else:
+                self.encoder = tf.layers.conv2d(
+                    self.encoder, filters = f, 
+                    kernel_size = k, strides = s, padding = 'same',
+                    activation = a)
+
+        self.encoder = tf.layers.flatten(self.encoder)
+
+        # -- Dense map to Latent Space ---------
+        # Note the sigmas has to be positive. Hence the sigmoid activation
+        self.mu    = tf.layers.dense(self.encoder, nLatent, activation=None)
+        self.sigma = tf.layers.dense(self.encoder, nLatent, activation=tf.nn.sigmoid)
+
+        # ------------------------------------------------------------
+        # Generate the decoder network
+        # ------------------------------------------------------------
+
+        #Here, self.Latent is simply a set of random normal distributions
+        # ------------------------------------------------------------
+        self.Latent = tf.placeholder( dtype=tf.float32, 
+                        shape=(None, nLatent))
+
+        # # self.decoder = self.Latent * self.LSigma + self.LMu 
+        self.decoder = self.Latent * self.sigma + self.mu 
+        
+        # # Generate a square function
+        self.decoder = tf.layers.dense(self.decoder, nLatent*nLatent, activation=None)
+        self.decoder = tf.reshape(self.decoder, (-1, nLatent, nLatent, 1))
+
+        for f, k, s, a in zip(reversed(filters), reversed(kernels), reversed(strides),
+                        reversed(activations) ):
+            # print(f, k, s, a)
+            self.decoder = tf.layers.conv2d_transpose(
+                    self.decoder, filters = f, 
+                    kernel_size = k, strides = s, padding = 'valid',
+                    activation = a)
+
+        # Only sane way of getting data back to the same shape
+        self.decoder  = tf.layers.flatten(self.decoder)
+        self.decoder1 = tf.layers.dense(self.decoder, nInpX*nInpY, activation=tf.nn.sigmoid)
+        
+        # # ------------------------------------------------------------
+        # # Generate the the cost functions
+        # # ------------------------------------------------------------
+        # # ----- Reconstruction Error ----------------
+        self.aeErr = self.Inp1 * tf.log( self.decoder1 + 1e-10 ) + (1-self.Inp1) * tf.log( 1 - self.decoder1 + 1e-10 )
+        self.aeErr = tf.reduce_sum( self.aeErr, 1)
+        self.aeErr = tf.reduce_mean( -1*self.aeErr )
+
+        # # ----- KL Divergence-------------------------
+        self.KLErr = tf.reduce_sum( self.sigma**2 + self.mu**2 - 1 - tf.log( self.sigma**2 ), 1)
+        self.KLErr = tf.reduce_mean( self.KLErr * 0.5 )
+
+        # # ----- Total Error-------------------------
+        self.Err   = (self.aeErr + self.L * self.KLErr)
+
+        # # ------------------------------------------------------------
+        # # Generate other misc operations
+        # # ------------------------------------------------------------
+        self.Opt  = tf.train.AdamOptimizer().minimize( self.Err )
+        self.init = tf.global_variables_initializer()
+        
+        self.saver = tf.train.Saver(var_list=tf.trainable_variables())
+
+        return
+
+    @lD.log(logBase + '.AE.saveModel')
+    def saveModel(logger, self, sess):
+        '''[summary]
+        
+        [description]
+        
+        Arguments:
+            logger {[type]} -- [description]
+            self {[type]} -- [description]
+            sess {[type]} -- [description]
+        
+        Returns:
+            [type] -- [description]
+        '''
+
+        now = dt.now().strftime('%Y-%m-%d--%H-%M-%S')
+        modelFolder = '../models/{}'.format(now)
+        os.makedirs(modelFolder)
+
+        path = self.saver.save( sess, os.path.join( modelFolder, 'model.ckpt' ) )
+        self.restorePoints.append(path)
+
+        return path
+
+    @lD.log(logBase + '.AE.getLatent')
+    def getLatent(logger, self, X, restorePoint=None):
+        try:
+            with tf.Session() as sess:
+                sess.run(self.init)
+
+                # Try to restore an older checkpoint
+                # ---------------------------------------
+                if restorePoint is not None:
+                    try:
+                        self.saver.restore(sess, restorePoint)
+                    except Exception as e:
+                        logger.error('Unable to restore the session at [{}]:{}'.format(
+                            restorePoint, str(e)))
+
+
+                mu, sigma = sess.run([self.mu, self.sigma], 
+                                feed_dict = { self.Inp : X} )
+
+                return mu, sigma   
+        except Exception as e:
+            logger.error('Unable to fit the model: {}'.format( str(e) ))
+
+        return
+
+    @lD.log(logBase + '.AE.fit')
+    def fit(logger, self, X, Niter=101, restorePoint=None):
+        '''[summary]
+        
+        [description]
+        
+        Decorators:
+            lD.log
+        
+        Arguments:
+            logger {[type]} -- [description]
+            self {[type]} -- [description]
+        
+        Keyword Arguments:
+            Niter {number} -- [description] (default: {101})
+            restorePoint {[type]} -- [description] (default: {None})
+        '''
+
+        try:
+            with tf.Session() as sess:
+                sess.run(self.init)
+
+                # Try to restore an older checkpoint
+                # ---------------------------------------
+                if restorePoint is not None:
+                    try:
+                        self.saver.restore(sess, restorePoint)
+                    except Exception as e:
+                        logger.error('Unable to restore the session at [{}]:{}'.format(
+                            restorePoint, str(e)))
+
+
+                mu, sigma = sess.run([self.mu, self.sigma], 
+                                feed_dict = { self.Inp : X} )
+
+                # print('Initial Latent State mean:')
+                # print(mu.mean(axis=0))
+
+                for i in tqdm(range(Niter)):
+
+                    latent = np.random.normal( 0, 1, mu.shape )
+
+                    _, aeError, KLErr, Err = sess.run(
+                            [self.Opt, self.aeErr, self.KLErr, self.Err], 
+                            feed_dict = {
+                                self.Inp    : X,
+                                self.Latent : latent})
+
+
+                mu, sigma = sess.run([self.mu, self.sigma], 
+                                feed_dict = { self.Inp : X} )
+                print('Final Latent State mean:')
+                print(mu.mean(axis=0))
+
+                self.saveModel(sess)
+        except Exception as e:
+            logger.error('Unable to fit the model: {}'.format( str(e) ))
+
+        return
+
+    @lD.log(logBase + '.AE.predict')
+    def predict(logger, self, X, restorePoint=None):
+        try:
+            with tf.Session() as sess:
+                sess.run(self.init)
+
+                # Try to restore an older checkpoint
+                # ---------------------------------------
+                if restorePoint is not None:
+                    try:
+                        self.saver.restore(sess, restorePoint)
+                    except Exception as e:
+                        logger.error('Unable to restore the session at [{}]:{}'.format(
+                            restorePoint, str(e)))
+
+                mu     = sess.run(self.mu, feed_dict = {self.Inp : X})
+                latent = np.random.normal( 0, 1, mu.shape )
+                xHat   = sess.run(self.decoder1, 
+                        feed_dict = {
+                            self.Inp    : X,
+                            self.Latent : latent })
+
+                return xHat
+                
+        except Exception as e:
+            logger.error('Unable to fit the model: {}'.format( str(e) ))
+
+        return
+
+    @lD.log(logBase + '.AE.predict2D')
+    def predict2D(logger, self, X, restorePoint=None):
+        try:
+            with tf.Session() as sess:
+                sess.run(self.init)
+
+                # Try to restore an older checkpoint
+                # ---------------------------------------
+                if restorePoint is not None:
+                    try:
+                        self.saver.restore(sess, restorePoint)
+                    except Exception as e:
+                        logger.error('Unable to restore the session at [{}]:{}'.format(
+                            restorePoint, str(e)))
+
+                mu     = sess.run(self.mu, feed_dict = {self.Inp : X})
+                latent = np.random.normal( 0, 1, mu.shape )
+                xHat   = sess.run(self.decoder, 
+                        feed_dict = {
+                            self.Inp    : X,
+                            self.Latent : latent })
+
+                return xHat
+                
+        except Exception as e:
+            logger.error('Unable to fit the model: {}'.format( str(e) ))
+
+        return
---
+++ src/modules/ConvVAE1/ConvVAE1.py
@@ -0,0 +1,124 @@
+import json, os
+import numpy      as np
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+
+from lib  import ConvVAELib1 
+from logs import logDecorator as lD 
+from tqdm import tqdm 
+
+from datetime import datetime as dt
+
+
+config = json.load(open('../config/config.json'))
+logBase = config['logging']['logBase'] + '.modules.ConvVAE1.ConvVAE1'
+
+@lD.log(logBase + '.testVAE1')
+def testConvVAE(logger):
+    '''print a line
+    
+    This function simply prints a single line
+    
+    Parameters
+    ----------
+    logger : {logging.Logger}
+        The logger function
+    '''
+
+    folder     = '../data/raw/mnist'
+    file       = 't10k-images-idx3-ubytenpy.npy'
+    labelsFile = 't10k-labels-idx1-ubytenpy.npy'
+    now        = dt.now().strftime('%Y-%m-%d--%H-%M-%S')
+    imgFolder  = '../results/img/{}'.format(now)
+    os.makedirs(imgFolder)
+
+    nInpX, nInpY, nInpCh = 28, 28, 1
+    X = np.load(os.path.join(folder, file)).astype(np.float32)
+    X = X /255.0
+    X = X.reshape(-1, nInpX, nInpY, nInpCh)
+    print(X.shape, X.max(), X.min())
+
+    labels = np.load(os.path.join(folder, labelsFile))
+
+    # Parameters for the network
+    filters     = [1, 7, 10]
+    kernels     = [3, 4, 5]
+    strides     = [2, 2, 1]
+    activations = [tf.nn.relu, tf.nn.relu, tf.nn.relu]
+
+    nLatent     = 2
+
+    cVAE = ConvVAELib1.ConvVAE(nInpX, nInpY, nInpCh, 
+                filters, kernels, strides, activations, 
+                nLatent, L=1.5)
+
+    # X1 = X[:77, :, :, :]
+    # with tf.Session() as sess:
+    #     sess.run( cVAE.init )
+    #     encoder = sess.run( cVAE.encoder, feed_dict = {cVAE.Inp: X1})
+    #     mu      = sess.run( cVAE.mu, feed_dict = {cVAE.Inp: X1})
+
+    #     latent  = np.random.normal( 0, 1, (mu.shape) )
+
+    #     Err     = sess.run( cVAE.Err, feed_dict = {
+    #                     cVAE.Inp: X1, cVAE.Latent : latent})
+
+
+    # plt.figure(figsize=(5, 8))
+    # for i in tqdm(range(40)):
+    #     x, y = i//8, i%8
+    #     ax = plt.axes([ x*0.2, y*0.125 , 0.2, 0.125])
+    #     ax.imshow(X[i].reshape(28,28), cmap=plt.cm.gray)
+    #     ax.set_xticks([])
+    #     ax.set_yticks([])
+    
+    # plt.savefig('../results/img/conv/X.png')
+    # plt.close('all')
+
+    # for it in tqdm(range(1000)):
+
+    #     if it > 0:
+    #         cVAE.fit(X, Niter=10, restorePoint=cVAE.restorePoints[-1])
+    #     else:
+    #         cVAE.fit(X, Niter=10)
+
+    #     Xhat  = cVAE.predict(X[:40,:,:, :], cVAE.restorePoints[-1])
+    #     Xhat1 = cVAE.predict2D(X[:2,:,:, :], cVAE.restorePoints[-1])
+    #     tqdm.write('{}'.format(Xhat1.shape))
+        
+    #     plt.figure(figsize=(5, 8))
+
+    #     for i in tqdm(range(40)):
+    #         x, y = i//8, i%8
+    #         ax = plt.axes([ x*0.2, y*0.125 , 0.2, 0.125])
+    #         ax.imshow(Xhat[i].reshape(28,28), cmap=plt.cm.gray)
+    #         ax.set_xticks([])
+    #         ax.set_yticks([])
+        
+    #     plt.savefig('../results/img/conv/Xhat_{:04d}.png'.format(it))
+    #     plt.close('all')
+
+    return
+
+
+
+
+@lD.log(logBase + '.main')
+def main(logger):
+    '''main function for module1
+    
+    This function finishes all the tasks for the
+    main function. This is a way in which a 
+    particular module is going to be executed. 
+    
+    Parameters
+    ----------
+    logger : {logging.Logger}
+        The logger function
+    '''
+
+    testConvVAE()
+
+    return
+
